3ef0d78526db585d62cd589145e9c8fce8c2af59
adity <aaditishinde1804@gmail.com>
2024-02-20 15:04:39 +0530

    Debugged

--- app/src/main/java/com/aditys/themusicalchemy/AlbumProfile.kt
+++ app/src/main/java/com/aditys/themusicalchemy/AlbumProfile.kt
@@ -4,11 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 
-class AlbumProfile : AppCompatActivity(){
+class AlbumProfile : AppCompatActivity() {
 
-   override fun onCreate(savedInstanceState: Bundle?){
-       super.onCreate(savedInstanceState)
-       setContentView(R.layout.album_profile)
-       var NavController: NavController= findNavController(R.id.)
-   }
 }
